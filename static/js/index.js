fbb5c618d39acf47b52a3d70796ed75f20255c6c
yuanyu <yuanyu90221@gmail.com>
2018-09-21 11:33:27 +0800

    modulize

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,26 @@
 // alternative to load event
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
-    let mailFormat = document.querySelectorAll("[name='mailFormat']");
+    // init default Setup
+    initFunction();
+    // bind Radios
+    bindRadios();
+    // bind Submit
+    bindSubmit();
+  }
+}
+
+const bindSubmit = () => {
+  let contactForm = document.querySelector('#contact-form');
+  contactForm.onsubmit = (event) => {
+    event.preventDefault();
+    console.log('submit');
+    console.log(event);
+  };
+};
+
+const initFunction = () => {
+  // let mailFormat = document.querySelectorAll("[name='mailFormat']");
     let initNumber = document.querySelector("[name='inputAmount']");
     initNumber.disabled=true;
     // initNumber.style= "display:none;";
@@ -14,45 +33,46 @@ document.onreadystatechange = function () {
     body.classList.remove('background_black');
     let head_title = document.querySelector('[name=head_title]');
     head_title.classList.remove('color_white');
-    mailFormat.forEach(node=> {
-      node.addEventListener('click', (e) => {
-        let currentValue = document.querySelector("[name='mailFormat']:checked");
-        let emailFormat = currentValue.value;
-        
-        let form_sender = document.querySelector("#form_sender");
-        let form_receiver = document.querySelector('#form_receiver');
-        let inputAmount = document.querySelector("[name='inputAmount']");
-      
-        form_sender.placeholder = emailFormat == "normal"?"Please enter Sender Email*":"Please enter Sender Address";
-        form_receiver.placeholder = emailFormat == "normal"?"Please enter Receiver Email*":"Please enter Receiver Address";
-        inputAmount.disabled = (emailFormat == "normal");
-        //inputAmount.style= (emailFormat == "normal")? "display:none;":"";
-        let labels1 = document.getElementsByTagName('label');
-        for (let i = 0 ; i < labels1.length; i++) {
-          if (emailFormat == "normal") {
-            labels1[i].classList.remove('color_white');
-          }
-          else {
-            labels1[i].classList.add('color_white');
-          }
-        }
+};
 
-        let bodyInner = document.body;
-        if (emailFormat=="normal") {
-          bodyInner.classList.remove('background_black');
-        } else {
-          bodyInner.classList.add('background_black');
+const bindRadios = () => {
+  let mailFormat = document.querySelectorAll("[name='mailFormat']");
+  mailFormat.forEach(node=> {
+    node.addEventListener('click', (e) => {
+      let currentValue = document.querySelector("[name='mailFormat']:checked");
+      let emailFormat = currentValue.value;
+      
+      let form_sender = document.querySelector("#form_sender");
+      let form_receiver = document.querySelector('#form_receiver');
+      let inputAmount = document.querySelector("[name='inputAmount']");
+    
+      form_sender.placeholder = emailFormat == "normal"?"Please enter Sender Email*":"Please enter Sender Address";
+      form_receiver.placeholder = emailFormat == "normal"?"Please enter Receiver Email*":"Please enter Receiver Address";
+      inputAmount.disabled = (emailFormat == "normal");;
+      let labels1 = document.getElementsByTagName('label');
+      for (let i = 0 ; i < labels1.length; i++) {
+        if (emailFormat == "normal") {
+          labels1[i].classList.remove('color_white');
         }
-        let head_titleInner = document.querySelector('[name=head_title]');
-        console.log(head_titleInner.innerHTML);
-        if (emailFormat=="normal") {
-          head_titleInner.classList.remove('color_white');
-          head_titleInner.innerHTML = "Normal Email";
-        } else {
-          head_titleInner.classList.add('color_white');
-          head_titleInner.innerHTML = "Zen Email";
+        else {
+          labels1[i].classList.add('color_white');
         }
-      });
+      }
+
+      let bodyInner = document.body;
+      if (emailFormat=="normal") {
+        bodyInner.classList.remove('background_black');
+      } else {
+        bodyInner.classList.add('background_black');
+      }
+      let head_titleInner = document.querySelector('[name=head_title]');
+      if (emailFormat=="normal") {
+        head_titleInner.classList.remove('color_white');
+        head_titleInner.innerHTML = "Normal Email";
+      } else {
+        head_titleInner.classList.add('color_white');
+        head_titleInner.innerHTML = "Zen Email";
+      }
     });
-  }
-}
+  });
+};
