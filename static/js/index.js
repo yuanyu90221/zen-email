75d5bd4bd09925ed2993073965edacc40cbdd4f0
yuanyu <yuanyu90221@gmail.com>
2018-09-21 11:48:13 +0800

    change radio control

--- static/js/index.js
+++ static/js/index.js
@@ -36,43 +36,53 @@ const initFunction = () => {
 };
 
 const bindRadios = () => {
+  // get radios
   let mailFormat = document.querySelectorAll("[name='mailFormat']");
-  mailFormat.forEach(node=> {
-    node.addEventListener('click', (e) => {
+  // change the current setup by click radio
+  mailFormat.forEach(radio=> {
+    radio.addEventListener('click', (e) => {
       let currentValue = document.querySelector("[name='mailFormat']:checked");
       let emailFormat = currentValue.value;
-      
-      let form_sender = document.querySelector("#form_sender");
-      let form_receiver = document.querySelector('#form_receiver');
-      let inputAmount = document.querySelector("[name='inputAmount']");
-    
-      form_sender.placeholder = emailFormat == "normal"?"Please enter Sender Email*":"Please enter Sender Address";
-      form_receiver.placeholder = emailFormat == "normal"?"Please enter Receiver Email*":"Please enter Receiver Address";
-      inputAmount.disabled = (emailFormat == "normal");;
-      let labels1 = document.getElementsByTagName('label');
-      for (let i = 0 ; i < labels1.length; i++) {
-        if (emailFormat == "normal") {
-          labels1[i].classList.remove('color_white');
-        }
-        else {
-          labels1[i].classList.add('color_white');
-        }
-      }
-
-      let bodyInner = document.body;
-      if (emailFormat=="normal") {
-        bodyInner.classList.remove('background_black');
-      } else {
-        bodyInner.classList.add('background_black');
-      }
-      let head_titleInner = document.querySelector('[name=head_title]');
-      if (emailFormat=="normal") {
-        head_titleInner.classList.remove('color_white');
-        head_titleInner.innerHTML = "Normal Email";
-      } else {
-        head_titleInner.classList.add('color_white');
-        head_titleInner.innerHTML = "Zen Email";
-      }
+      // setup input by emailFormat
+      setupInputByFormat(emailFormat);
+      // change labels by emailFormat
+      changeLabelsByFormat(emailFormat);
+      // set up bg by emailFormat
+      setupBgByFormat(emailFormat);
     });
   });
 };
+
+const changeLabelsByFormat = (emailFormat) => {
+  let labels = document.getElementsByTagName('label');
+  for (let i = 0 ; i < labels.length; i++) {
+    if (emailFormat == "normal") {
+      labels[i].classList.remove('color_white');
+    }
+    else {
+      labels[i].classList.add('color_white');
+    }
+  }
+};
+
+const setupInputByFormat = (emailFormat) => {
+  let form_sender = document.querySelector("#form_sender");
+  let form_receiver = document.querySelector('#form_receiver');
+  let inputAmount = document.querySelector("[name='inputAmount']");
+  form_sender.placeholder = emailFormat == "normal"?"Please enter Sender Email*":"Please enter Sender Address";
+  form_receiver.placeholder = emailFormat == "normal"?"Please enter Receiver Email*":"Please enter Receiver Address";
+  inputAmount.disabled = (emailFormat == "normal");
+};
+
+const setupBgByFormat = (emailFormat) => {
+  let head_titleInner = document.querySelector('[name=head_title]');
+  let bodyInner = document.body;
+  if (emailFormat=="normal") {
+    bodyInner.classList.remove('background_black');
+    head_titleInner.classList.remove('color_white');
+  } else {
+    bodyInner.classList.add('background_black');
+    head_titleInner.classList.add('color_white');
+  }
+  head_titleInner.innerHTML = emailFormat=="normal"?"Normal Email":"Zen Email";
+}
